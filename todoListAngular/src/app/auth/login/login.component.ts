dbca3b40da414170addc09d7b2d593379b52270f
Andysm21 <102580958+Andysm21@users.noreply.github.com>
2024-09-23 16:16:56 +0300

    Added Login/Signup MultiStepper Forms

--- todoListAngular/src/app/auth/login/login.component.ts
+++ todoListAngular/src/app/auth/login/login.component.ts
@@ -1,13 +1,14 @@
 
 import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
-import {FormControl, FormsModule, NgForm, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatStepperModule} from '@angular/material/stepper';
 import {MatCardModule} from '@angular/material/card';
-import {catchError, merge, of, tap} from 'rxjs';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {catchError, merge, of, tap} from 'rxjs';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { TaskService } from '../../task/task-service';
@@ -15,17 +16,24 @@ import { TaskService } from '../../task/task-service';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule,MatCardModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule,MatCardModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatIconModule,MatStepperModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 
 export class LogInComponent {
+  form = new FormArray<FormGroup>([
+    new FormGroup({ email: new FormControl("", { validators: [Validators.required, Validators.email,
+      Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+    ]}) }),
+    new FormGroup({ password: new FormControl("", { validators: [Validators.required,Validators.minLength(6)]}) }),
+  ]);
 
   submit=false;
-  readonly email = new FormControl('', [Validators.required, Validators.email]);
-  readonly password = new FormControl('', [Validators.required,Validators.minLength(6)]);
+  readonly email = this.form.at(0).get('email') as FormControl;
+  readonly password = this.form.at(1).get('password') as FormControl;
+
   errorMessage = signal('');
   hide = signal(true);
 
@@ -48,13 +56,19 @@ export class LogInComponent {
       this.errorMessage.set('You must enter a value');
     } else if (this.email.hasError('email')) {
       this.errorMessage.set('Not a valid email');
-    }else {
-      this.errorMessage.set('');
+    }else if (this.password.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
     }
+    else if (this.email.hasError('pattern')){
+      this.errorMessage.set('Not a valid email');
+    }
+    else if (this.password.hasError('minlength')) {
+      this.errorMessage.set('Password must be at least 6 characters');
+    } else {
+      this.errorMessage.set('');}
   }
-  onSubmit(authForm: NgForm) {
-    this.submit = !this.submit;
-
+  onSubmit() {
+    this.submit=!this.submit
     if (this.email.valid && this.password.valid) {
       this.authService.signIn(this.email.value!, this.password.value!).pipe(
         tap(response => {
@@ -74,6 +88,6 @@ export class LogInComponent {
     } else {
       console.log(this.errorMessage());
     }
-    authForm.reset();
+
   }
 }
